b86e22cf4d49db9479379b98f2ad70268dd96c3a
DRON <captain.destroyer@yandex.ru>
2025-02-22 02:21:04 +0700

    fix type currentClient

--- src/pages/Clients/Clients.tsx
+++ src/pages/Clients/Clients.tsx
@@ -8,7 +8,7 @@ import styles from './Clients.module.scss';
 function ClientsPage() {
   const [searchParams] = useSearchParams();
 
-  const [currentClient, setCurrentClient] = useState<string | null>(searchParams.get('client') || null);
+  const [currentClient, setCurrentClient] = useState<React.Dispatch<React.SetStateAction<string | null>>>(searchParams.get('client') || null);
 
   return (
     <AdminLayout title="Список клиентов">
